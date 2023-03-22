10265138ebdf468ec9972b11140ed234d61e7224
Vsevolod Demidov <fondad@ukr.net>
2023-03-22 10:26:52 +0100

    URLS were changed, model Articles was created

--- mygis/urls.py
+++ mygis/urls.py
@@ -1,7 +1,7 @@
-"""djangoProject URL Configuration
+"""GIST URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,28 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-"""
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-"""
-
-# from django.contrib import admin
-# from django.conf.urls import url
-
 
 from django.urls import path, re_path
 from mygis import views
 
 urlpatterns = [
-    re_path('', views.home, name='home'),
+    re_path('^$', views.home, name='home'),
 ]
-'''
-
-urlpatterns = [
-    url(r'^$', views.home, name='home'),
-]
-'''
